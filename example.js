d2a5ae58902cccffdd3e14cda8729d4a196038c8
Vincent Voyer <vincent@codeagain.com>
2022-05-16 00:01:47 +0200

    8 or 16, no idea but length is better with 8

--- example.js
+++ example.js
@@ -6,17 +6,20 @@ const originalIron = require('@hapi/iron');
 iron.seal({
     hello: 'world'
 }, 'some_not_random_password_that_is_at_least_32_characters', iron.defaults).then(async (seal) => {
-    console.log(seal);
+    console.log('');
+    console.log(seal, 'new');
     const data = await iron.unseal(seal, 'some_not_random_password_that_is_at_least_32_characters', iron.defaults);
     console.log(data);
 });
 
-// originalIron.seal({
-//     hello: 'world'
-// }, 'some_not_random_password_that_is_at_least_32_characters', originalIron.defaults).then(async (seal) => {
-//     console.log(seal);
-//     // const data = await iron.unseal(seal, 'some_not_random_password_that_is_at_least_32_characters', iron.defaults);
-//     // console.log(data);
-// });
+originalIron.seal({
+    hello: 'world'
+}, 'some_not_random_password_that_is_at_least_32_characters', originalIron.defaults).then(async (seal) => {
+    console.log('');
+
+    console.log(seal, 'old');
+    // const data = await iron.unseal(seal, 'some_not_random_password_that_is_at_least_32_characters', iron.defaults);
+    // console.log(data);
+});
 
 
